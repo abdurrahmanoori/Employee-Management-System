b1dbd2f6e4ec0f5dfbcaa87638f3cf15ee96922d
Abudurrahman <Abdurrahmannoori10@gmail.com>
2022-09-02 16:33:09 +0430

    added employee class and repository Lecture 18

---
+++ EmployeeManagementSystem/Models/MockEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        public Employee GetEmployee(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
